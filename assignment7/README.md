ff307e66b55be84ee68ac92e08e7f0dd0fc924ba
Your Name <yourEmail@cooper.edu>
2023-12-08 09:05:03 -0500

    assignment 7

---
+++ assignment7/README.md
@@ -0,0 +1,2 @@
+Each comment in `problems.py` is a problem, write code for each comment
+completing the task.
---
+++ assignment7/problems.py
@@ -0,0 +1,61 @@
+import random
+import string
+
+# Create a comment with your name
+
+# Create a string called 'name' which stores your name
+
+# Print the 'name' variable
+
+# Create a variable storing an integer
+
+# Create a variable storing a floating point number
+
+# This line creates a string of 1000000 random letters
+rs = ''.join(random.choices(string.ascii_lowercase, k=1000001))
+# Print whether your first name (lowercase) appears in the string
+
+# Print the first letter, last letter, and middle letter
+
+# This line creates a string of some random words
+rw = ''.join(random.choices(list(string.ascii_lowercase) + [' '], k=1000))
+# Print the number of words (a word being characters surrounded by spaces)
+
+# This is a string to be formatted
+sentence = '{greeting}, my name is {name}'
+# Print the sentence formatted with your choice of greeting and your name
+
+# Create a variable storing a tuple of your three favorite numbers
+
+# Assign those three numbers to variables 'a', 'b', and 'c' in one line
+
+# Create a variable storing None
+
+# Create a variable, lst, storing a list of the numbers 0-1000 (inclusive)
+
+# Print the sum and average of the list
+
+# Create a variable, ascii_low, storing a **set** of all lowercase ascii characters
+
+# Create a varaible, ascii_all, storing a **set** of all ascii characters
+
+# Create a variable, ascii_hi, storing the set different between ascii_all and
+# ascii_low, i.e. (ascii_all - ascii_low)
+
+# Create a dictionary with one key, your first name that maps to one value,
+# your last name
+
+# Alter the dictionary to make your last name all caps
+
+# Here is a nested structure
+d = {'hello': [[{'cory': 'nezin'}, {'CORY': ['N', 'E', 'Z', 'I', 'N']}]]}
+# Change the 'Z' from the list ['N', 'E', 'Z', 'I', 'N'] inside to a 'z'
+
+# Use a for loop to print numbers 1 through 10 (inclusive)
+
+# Use a for loop to create a list of the first 10 cubes (0 through 9, cubed)
+
+# Now use a list comprehension to do the same
+
+# Write a function 'unique' which takes a list and checks if each element is
+# unique, meaning there are no duplicates inside the list.
